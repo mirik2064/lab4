f2447096e8d7364f35748d5da9c6211865c6eadd
mirik2064 <miroslav.dudnik2005@gmail.com>
2023-04-12 20:39:23 +0200

    Updated .gitignore to ignore log files

--- script.sh
+++ script.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] || [[ $1 == "--help" ]]
-then
-	echo "Usage:"
-	echo "script.sh --date : Display today's date"
-    echo "script.sh --logs [n] : Create n (default 100) log files with names logx.txt and write to them their name, the script that created them, and the current date"
-	echo "script.sh --help : Display this help message"
+if [[ $# -eq 0 || $1 == "--help" ]]; then
+	echo "Usage script.sh [OPTIONS]:"
+	echo "	-h, --help        Display this help message"
+	echo "	-d, --date        Display today's date"
+    echo "	-l, --logs [N]    Create N (default 100) log files with names logx.txt"
+    echo "	-e, --error [N]   Create N (default 100) error files"
+    echo "	--init [link]     Clone the repository to the current directory and add the script directory to PATH"
 	exit 0
 fi
 
-if [[ $1 == "--date" ]]
-then
+if [[ $1 == "--date" || $1 == "-d" ]]; then
 	date +%Y-%m-%d
-elif [[ $1 == "--logs" ]]
-then
+
+elif [[ $1 == "--init" ]]; then
+	git clone $2
+	echo 'export PATH="$PATH:'$(pwd)'"' >> ~/.bashrc
+    source ~/.bashrc
+
+elif [[ $1 == "--error" || $1 == "-e" ]]; then
 	n=${2:-100}
-    for (( i =  1; i <= $n; i++ ))
-	do
+	
+	for (( i =  1; i <= $n; i++ )) do
+		mkdir -p "error$i"
+        echo "Filename: error$i.txt" > "error$i/error$i.txt"
+        echo "Created by script: $0" >> "error$i/error$i.txt"
+        echo "Creation date: $(date)" >> "error$i/error$i.txt"
+	done
+
+elif [[ $1 == "--logs" || $1 == "-l" ]]; then
+	n=${2:-100}
+
+    for (( i =  1; i <= $n; i++ )) do
 		filename="log$i.txt"
 		echo "Filename: $filename" >> $filename
 		echo "Created by script: $0" >> $filename
 		echo "Creation date: $(date +%Y-%m-%d)" >> $filename
     done
+
 else
 	echo "Invalid option: $1"
 	echo "Use script.sh --help to display available options"
-fi
-
-if [[ -d ".git" ]]
-then
-	echo "Adding .gitignore file"
-	echo "*log*" > .gitignore
-	git add .gitignore
-	git commit -m "Add .gitignore file"
-	git push
-	git checkout -b taskBranch
-	git add .
-	git commit -m "Add logs and .gitignore"
-	git push
-	git checkout main
-	git merge taskBranch
-	git tag v1.0
-	git push --tags
 fi
